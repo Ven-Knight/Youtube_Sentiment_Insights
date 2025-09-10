b5a0c7c9531752d0f301d202d051ec3b66eb2d44
Ven-Knight <venkatareddy.nalamalapu@gmail.com>
2025-09-10 19:34:57 +0530

    Updated API with prod server URL

--- yt-chrome-plugin-frontend/popup.js
+++ yt-chrome-plugin-frontend/popup.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const API_KEY = 'AIzaSyDW7TYt45XqhHHG0XM4sfUd7cMr99y9iQw';  // Actual YouTube Data API key
   // const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';   
   // const API_URL = 'http://localhost:5000/';
-  const API_URL = 'http://localhost:5000/';
+  const API_URL = 'http://15.206.92.37:8080/';
 
   // Get the current tab's URL
   chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
